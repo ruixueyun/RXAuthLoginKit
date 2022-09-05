a5d2f585a7c8d677f20cbaa9f2d0accf0c0b6698
chenhan <chenhan@jixiang.cn>
2022-09-05 15:58:17 +0800

    1.0.0

---
+++ RXAuthLoginKit.framework/Headers/RXAuthLoginService.h
@@ -0,0 +1,33 @@
+//
+//  RXAuthLoginService.h
+//  RXAuthLoginKit
+//
+//  Created by 陈汉 on 2022/5/14.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RXAuthLoginService : NSObject
+
+/**
+ * 获取SDK实例（单例）
+ */
++ (instancetype)sharedSDK;
+
+/**
+ * 一键登录弹窗
+ * @param privacy1 用户协议url
+ * @param privacy2 隐私协议url
+ * @param callback 登录回调
+ * success为YES，将accessToken返回值传入登录接口的extDic中
+ * success为NO视为授权失败
+ */
+- (void)setAuthLoginViewWithPrivacy1:(NSString *)privacy1
+                            privacy2:(NSString *)privacy2
+                            callBack:(void(^)(NSString *accessToken, BOOL success))callBack;
+
+@end
+
+NS_ASSUME_NONNULL_END
