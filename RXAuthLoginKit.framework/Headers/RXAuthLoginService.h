5278a58b5eb487636ca6f6c81373441edf684515
chenhan <chenhan@jixiang.cn>
2022-09-28 15:48:21 +0800

    1.0.1

--- RXAuthLoginKit.framework/Headers/RXAuthLoginService.h
+++ RXAuthLoginKit.framework/Headers/RXAuthLoginService.h
@@ -21,11 +21,13 @@ NS_ASSUME_NONNULL_BEGIN
  * @param privacy1 用户协议url
  * @param privacy2 隐私协议url
  * @param callback 登录回调
+ * @param atauth_appkey 一键登录appkey
  * success为YES，将accessToken返回值传入登录接口的extDic中
  * success为NO视为授权失败
  */
 - (void)setAuthLoginViewWithPrivacy1:(NSString *)privacy1
                             privacy2:(NSString *)privacy2
+                       atauth_appkey:(NSString *)atauth_appkey
                             callBack:(void(^)(NSString *accessToken, BOOL success))callBack;
 
 @end
